7014a7293392ecdcac961fceeb66a02f1bdaf3da
Petro Podrezo <petro@podrezo.com>
2025-08-29 23:58:48 -0400

    Use payload to send in target URL instead of config

--- handler.js
+++ handler.js
@@ -1,23 +1,32 @@
 const ConnectionTester = require('./test-connection');
-const config = require('./config.json');
 const SNSPublisher = require('./sns-publisher');
 
-// Read SNS topic ARN from environment variable instead of config
+// Read SNS topic ARN from environment variable
 const snsTopicArn = process.env.SNS_TOPIC_ARN;
+
 if (!snsTopicArn) {
   throw new Error('SNS_TOPIC_ARN environment variable is not set');
 }
 
 const snsPublisher = new SNSPublisher(snsTopicArn);
 
-exports.run = async () => {
-  const statusInfo = await ConnectionTester.runTest(config.targetUrl, config.timeoutMs);
+exports.run = async (event, context) => {
+  const remainingTimeMs = context.getRemainingTimeInMillis();
+  const timeoutMs = remainingTimeMs - 1000;
+  // Read target URL from event payload
+  const targetUrl = event.targetUrl;
+
+  if (!targetUrl) {
+    throw new Error('targetUrl is required in the event payload');
+  }
+
+  const statusInfo = await ConnectionTester.runTest(targetUrl, timeoutMs);
 
   console.log(statusInfo.message);
 
   // Only send SNS notification if the test result indicates a failure
   if (statusInfo.isFailure) {
-    await snsPublisher.publishHealthCheckResult(statusInfo, config.targetUrl);
+    await snsPublisher.publishHealthCheckResult(statusInfo, targetUrl);
   }
 
   return statusInfo.message;
