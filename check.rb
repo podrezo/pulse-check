7c3e4d1ba12e22eccec5656be411c83ca6b63c8d
Petro Podrezo <petro@podrezo.com>
2025-09-02 23:04:56 -0400

    Publish CloudWatch metric on failure

--- check.rb
+++ check.rb
@@ -1,6 +1,7 @@
 # nethttp.rb
 require 'uri'
 require 'net/http'
+require 'aws-sdk-cloudwatch'
 require_relative './result'
 
 # The amount of time, in milliseconds, that is left for function execution (outside of the HTTP timeout)
@@ -12,6 +13,12 @@ def handler(event:, context:)
 
   result = web_request(uri_to_check, http_timeout)
   puts result.to_s
+
+  # Publish CloudWatch metric on failure
+  unless result.success?
+    publish_failure_metric(uri_to_check.host)
+  end
+
   { success: result.success?, result: result.to_s }
 end
 
@@ -27,4 +34,31 @@ rescue Net::OpenTimeout, Net::ReadTimeout, Net::HTTPBadResponse, Net::HTTPHeader
   Result.new(:connection_problem, e.message)
 rescue StandardError => e
   Result.new(:error, "#{e.class}: #{e.message}\n#{e.backtrace&.join("\n")}")
+end
+
+def publish_failure_metric(hostname)
+  cloudwatch = Aws::CloudWatch::Client.new
+
+  cloudwatch.put_metric_data({
+    namespace: 'PulseCheck',
+    metric_data: [
+      {
+        metric_name: 'HTTPRequestFailures',
+        dimensions: [
+          {
+            name: 'Hostname',
+            value: hostname
+          }
+        ],
+        value: 1.0,
+        unit: 'Count',
+        timestamp: Time.now
+      }
+    ]
+  })
+
+  puts "Published failure metric for #{hostname}"
+rescue => e
+  puts "Failed to publish CloudWatch metric: #{e.message}"
+  # Don't re-raise - we don't want CloudWatch failures to break the pulse check
 end
